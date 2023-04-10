fd1a29ce4a7d574966aa3b9a6bd5a7a6d793463a
201steve <jeongillee201@gmail.com>
2023-04-11 08:16:13 +0900

    auth 기능추가 - api주소 관리용 파일 추가 - 토큰 유효하지않다면 로그인페이지로 리다이렉트 추가

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Todo from "./Todo";
+import { api } from "../../api/api";
 
 const List = () => {
   const [todoList, setTodoList] = useState([]);
+  const navigate = useNavigate();
 
-  const getTodo = async (url) => {
-    const response = await fetch(url);
+  const getTodo = async (url, option) => {
+    const response = await fetch(url, option);
     const todoData = await response.json();
     setTodoList(todoData.data);
   };
 
+  const goToSignIn = () => {
+    navigate("signin");
+  };
+
   useEffect(() => {
-    getTodo("/mock/getTodo.json");
+    if (!localStorage.getItem("todo-token")) {
+      alert("로그인이 필요합니다");
+      navigate("/signin");
+    }
+    getTodo(api.getTodos, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json;charset=utf-8",
+        Authorization: localStorage.getItem("todo-token"),
+      },
+    });
   }, []);
 
   return (
     <div className="m-10 max-w-2xl border-2 rounded-lg pb-10">
       <div className="w-full text-right p-4">
-        <button className="text-md bg-zinc-100 rounded-lg px-3 py-1 mr-3 active:bg-zinc-300 active:text-white">
+        <button
+          onClick={goToSignIn}
+          className="text-md bg-zinc-100 rounded-lg px-3 py-1 mr-3 active:bg-zinc-300 active:text-white"
+        >
           Login
         </button>
         <button className="text-md bg-zinc-100 rounded-lg px-3 py-1 active:bg-zinc-300 active:text-white">
