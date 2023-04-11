5ce568fd73e93a9def5abcdeb0b3027708b45a5e
201steve <jeongillee201@gmail.com>
2023-04-12 08:51:59 +0900

    mofidy:detail view 수정 - label태그 삭제 - 체크박스에 체크 되야 detail이 보이던 방식에서 todo 클릭 하면 보이는것으로 수정 - detail 닫기 버튼 추가 - title의길이가 특정 index이상 되면 ... 으로 collapse되게 view 수정 - input scale transition 삭제

--- src/pages/list/Todo.jsx
+++ src/pages/list/Todo.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { CiEdit, CiSquareRemove } from "react-icons/ci";
 
-const Todo = ({ title, id, whichISelected, selectOrNot }) => {
+const Todo = ({ title, id, whichISelected, displayDetail }) => {
+  const makeCollapsedTitle = (index) => {
+    if (title.length < index) return title;
+
+    const startIndex = title.substring(0, index);
+    const concatedString = "...";
+    const collapsedTitle = startIndex + concatedString;
+    return collapsedTitle;
+  };
+
   return (
     <div
       onClick={(e) => {
         whichISelected(e, id);
       }}
-      className="flex items-center justify-center h-10 mb-5 group"
+      className="flex items-center justify-between h-10 mb-5 group"
     >
-      <label htmlFor={id} className="w-full h-10 ml-5">
-        <div className="flex items-center h-10 py-5 border-2 border-emerald-200 duration-150 active:scale-105 ">
-          <input id={id} type="checkbox" className="mx-5" onChange={selectOrNot} />
-          <p type="text" className=" focus:outline-none duration-150 focus:scale-110">
-            {title}
-          </p>
-        </div>
-      </label>
+      <div
+        onClick={displayDetail}
+        className="flex items-center w-full h-10 ml-5 py-5 border-2 rounded-lg border-emerald-200 active:bg-emerald-100"
+      >
+        <input type="checkbox" className="mx-5" />
+        <p type="text" className=" focus:outline-none duration-150 focus:scale-110">
+          {makeCollapsedTitle(20)}
+        </p>
+      </div>
+
       <div className=" flex items-center justify-center invisible group-hover:visible">
         <CiEdit className="duration-150 hover:scale-125 text-green-700 mr-3 " size="30px" />
         <CiSquareRemove className="duration-150 mr-5 hover:scale-125 text-red-700" size="30px" />
