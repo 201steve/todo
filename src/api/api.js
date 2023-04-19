1d73186070849a9bc89306ea08dcfa4690eb52fc
201steve <jeongillee201@gmail.com>
2023-04-19 11:02:50 +0900

    ADD: delete기능추가와 view수정 - delete 버튼을 누르면 todo 삭제 되는 기능 추가 - delete 버튼을 누르고 삭제되면 re-render되도록 setState 구성 - createTodo 하면re-render 되도록 setState수정 - update 용도 modal 구현중

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,10 @@ const BASE = "http://localhost:8080";
 
 export const api = {
   getTodos: `${BASE}/todos`,
-  getTodoById: `${BASE}/todos/:id`,
+  getTodoById: `${BASE}/todos/`,
   createTodo: `${BASE}/todos`,
-  updateTodo: `${BASE}/todos/:id`,
-  deleteTodo: `${BASE}/todos:id`,
+  updateTodo: `${BASE}/todos/`,
+  deleteTodo: `${BASE}/todos/`,
   signIn: `${BASE}/users/login`,
   signUp: `${BASE}/users/create`,
 };
--- src/pages/list/Todo.jsx
+++ src/pages/list/Todo.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiEdit, CiSquareRemove } from "react-icons/ci";
 
-const Todo = ({ title, id, whichISelected, displayDetail }) => {
+const Todo = ({ title, id, whichISelected, displayDetail, deleteTodo }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
   const makeCollapsedTitle = (index) => {
     if (title.length < index) return title;
 
@@ -11,13 +13,16 @@ const Todo = ({ title, id, whichISelected, displayDetail }) => {
     return collapsedTitle;
   };
 
+  const toggleEdit = () => {
+    setIsEdit((prev) => !prev);
+  };
+
+  const selectTodo = (e) => {
+    whichISelected(e, id);
+  };
+
   return (
-    <div
-      onClick={(e) => {
-        whichISelected(e, id);
-      }}
-      className="flex items-center justify-between h-10 mb-5 group"
-    >
+    <div onClick={selectTodo} className="flex items-center justify-between h-10 mb-5 group">
       <div
         onClick={displayDetail}
         className="flex items-center w-full h-10 ml-5 py-5 border-2 rounded-lg border-emerald-200 active:bg-emerald-100"
@@ -27,10 +32,33 @@ const Todo = ({ title, id, whichISelected, displayDetail }) => {
           {makeCollapsedTitle(20)}
         </p>
       </div>
+      <div className=" flex items-center justify-center ">
+        <CiEdit
+          onClick={toggleEdit}
+          className="duration-150 hover:scale-125 text-green-700 mr-3 "
+          size="30px"
+        />
+        {isEdit && (
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row items-center justify-center bg-zinc-500/70 w-full h-full ">
+            <div>
+              <input
+                placeholder="title"
+                className=" border-2 border-emerald-200 rounded-lg mb-10 h-14 w-3/4"
+              />
+              <input
+                placeholder="detail"
+                className=" border-2 border-emerald-200 rounded-lg h-14 w-3/4"
+              />
+            </div>
+            <button className="bg-zinc-100">닫기</button>
+          </div>
+        )}
 
-      <div className=" flex items-center justify-center invisible group-hover:visible">
-        <CiEdit className="duration-150 hover:scale-125 text-green-700 mr-3 " size="30px" />
-        <CiSquareRemove className="duration-150 mr-5 hover:scale-125 text-red-700" size="30px" />
+        <CiSquareRemove
+          onClick={(e) => deleteTodo(e, id)}
+          className="duration-150 mr-5 hover:scale-125 text-red-700"
+          size="30px"
+        />
       </div>
     </div>
   );
